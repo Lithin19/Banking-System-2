d265d4baae06aecef12ef40541856daa33933733
Miraa1510 <Sreemiraa.Tn@insightglobal.com>
2026-04-23 11:35:01 +0530

    customer service files added

--- src/main/java/com/bankingsystem/BankingSystem/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/bankingsystem/BankingSystem/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,8 @@
 package com.bankingsystem.BankingSystem.exceptions;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
-
-
-
